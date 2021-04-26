5379f81eb21626569336430020f28ab161764927
Jeffrey R. Gerber <jeffreygerber@gmail.com>
2021-04-26 00:08:03 +0000

    Isolate unix to freebsd and linux

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 #![warn(missing_docs)]
 #![deny(rust_2018_idioms)]
 
-#[cfg_attr(unix, path = "unix.rs")]
+#[cfg_attr(any(target_os = "linux", target_os = "freebsd"), path = "unix.rs")]
 #[cfg_attr(windows, path = "windows.rs")]
 #[cfg_attr(macos, path = "macos.rs")]
 mod platform;
