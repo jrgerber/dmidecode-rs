fb2e8fe1a6089e71b1c59dd0a3acd7b560b116cf
Juan <44718767+jczuluag@users.noreply.github.com>
2026-08-29 03:04:34 +0000

    enable test only for freebsd

--- tests/cli.rs
+++ tests/cli.rs
@@ -123,7 +123,7 @@ fn test_no_sysfs() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-#[cfg(any(target_os = "linux", target_os = "freebsd"))]
+#[cfg(any(target_os = "freebsd"))]
 #[test]
 fn test_dev_mem() -> Result<(), Box<dyn std::error::Error>> {
     // test good path to /dev/mem
