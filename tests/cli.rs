c531a3b940af1c66beaec4002f65d68926260768
Juan <44718767+jczuluag@users.noreply.github.com>
2026-08-29 03:31:49 +0000

    no-sysfs for freebsd

--- tests/cli.rs
+++ tests/cli.rs
@@ -113,7 +113,7 @@ fn test_oem_string_valid() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-#[cfg(any(target_os = "linux"))]
+#[cfg(any(target_os = "freebsd"))]
 #[test]
 fn test_no_sysfs() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd1 = Command::cargo_bin(CLI_COMMAND)?;
